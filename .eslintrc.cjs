39dd0a417ed23a6bbf3b2ab34f553202a3ba9d50
Erway <rudiwidodo069@gmail.com>
2023-07-05 10:49:03 +0700

    add eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,61 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-}
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'prettier',
+    '@typescript-eslint',
+    'simple-import-sort',
+    'tailwindcss'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        tabWidth: 2,
+        printWidth: 80,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        quoteProps: 'as-needed',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        jsxBracketSameLine: false,
+        endOfLine: 'lf'
+      }
+    ],
+    'react-refresh/only-export-components': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-undef': 'off',
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/no-custom-classname': 'warn'
+  }
+};
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # membuat setup template vite-react-typescript
 
 saya membuat template ini hanya untuk memudahkan saya dalam menggembangkan sebuah project yang akan saya bangun. dengan menggunakan template ini akan lebih mempersingkat waktu dalam penggembangan sebuah karena kita tidak lagi melakukan setup dari awal.
+
+## eslint dan prettier
+
+eslint: untuk memastikan kualitas penulisan kode.
+prettier: untuk memastikan kode terformat dengan baik berdasarkan aturan-aturan yang telah ditetapkan
